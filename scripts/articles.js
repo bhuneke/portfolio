63f1559c9815a26e1d4085f64fea152ebbfb7446
Brigitte Huneke <brigittehuneke@gmail.com>
2016-12-13 16:41:26 -0800

    blog articles added and sorted

---
+++ scripts/articles.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var articles = [];
+
+function Article (opts) {
+  this.title = opts.title;
+  this.category = opts.category;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
+}
+
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+  $newArticle.find('h2').html(this.title);
+  $newArticle.attr('data-category', this.category);
+  $newArticle.find('time[datetime]').attr('date', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
+  $newArticle.find('section.article-body').html(this.body);
+
+  $newArticle.removeAttr('class');
+
+  return $newArticle;
+};
+
+blogArticles.sort(function(curElem, nextElem) {
+  return (new Date(nextElem.publishedOn)) - (new Date(curElem.publishedOn));
+});
+
+blogArticles.forEach(function(ele) {
+  articles.push(new Article(ele));
+});
+
+console.log(articles);
+
+articles.forEach(function(a) {
+  $('#blog').append(a.toHtml());
+});
