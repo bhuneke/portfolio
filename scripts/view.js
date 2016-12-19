c8349874e6045b5ced3c11197ccfdcfa22646d9d
Brigitte Huneke <brigittehuneke@gmail.com>
2016-12-19 14:22:01 -0800

    worked on hover for nav items

--- scripts/view.js
+++ scripts/view.js
@@ -1,11 +1,6 @@
 'use strict';
 var view = {};
 
-view.handleLoad = function () {
-  $('.tab-content').hide();
-  $('#home').fadeIn();
-};
-
 view.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
     var whichTab = $(this).attr('data-content');
@@ -14,5 +9,10 @@ view.handleMainNav = function () {
   });
 };
 
-view.handleLoad();
+view.handleLoad = function () {
+  $('.tab-content').hide();
+  $('#home').fadeIn();
+};
+
 view.handleMainNav();
+view.handleLoad();
